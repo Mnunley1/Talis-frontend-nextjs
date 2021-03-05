97014770c39caf396085e866fafc19bb2be5ba33
Malcolm Nunley <Mxnunley1@gmail.com>
2021-03-05 17:36:23 -0500

    fixed tab text and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
-        <title>Talis</title>
+        <title>Talis | Luxury Apartments and Homes For Rent</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
